4080054654d479fa32cd8dbe99b4074ffb368656
PavelMIPT <104658715+PavelMIPT@users.noreply.github.com>
2023-12-05 21:35:07 +0300

    Delete lab_4/NoiseGenerator.m

--- lab_4/NoiseGenerator.m
+++
@@ -1,8 +0,0 @@
-function [NoisedSignal, Noise] = NoiseGenerator(signal, SNR)
-    % Определим амплитуду шума 
-    % A_signal = 1
-    A_noise = 10^(-SNR / 20);
-
-    Noise = A_noise * normrnd(0, 1, [1, size(signal, 2)]);
-    NoisedSignal = signal + Noise;
-end
